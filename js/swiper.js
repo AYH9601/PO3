9b9b2e52d78d2fec48e3cfa0f6b9cf1d2c0245f4
ayh9601 <ays9499@naver.com>
2022-02-26 04:52:22 +0900

    stage8

--- js/swiper.js
+++ js/swiper.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 0,
     // loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
     scrollbar: {
         el: ".swiper-scrollbar",
         hide: true,
